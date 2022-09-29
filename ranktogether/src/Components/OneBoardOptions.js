f0e4a7d7701d4981219affbe0a5cd4488c5251c1
Flordeliza Canlas <maemaeperlas@gmail.com>
2022-09-29 14:51:48 -0700

    Fixed the drag and drop functionality!

--- ranktogether/src/Components/OneBoardOptions.js
+++ ranktogether/src/Components/OneBoardOptions.js
@@ -1,21 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom"
 import { Header, Grid, Image, Card, Dimmer, Loader } from "semantic-ui-react"
+import OptionsCards from "./OptionsCard";
+import update from 'immutability-helper'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 
 function OneBoardOptions() {
 
     const params = useParams();
     const boardID = parseInt(params.id)
     const [board, setBoard] = useState(null)
+    const [options, setOptions] = useState([]);
 
     useEffect(() => {
         fetch(`/boards/${boardID}`)
             .then(data => data.json())
-            .then(data => setBoard(data))
+            .then(data => {
+                setBoard(data)
+                setOptions(data.options)
+            })
     }, [])
 
     function calculateColumns() {
-        const boardLength = board.options.length
+        const boardLength = options.length
         if (boardLength === 2 || boardLength === 4) {
             return 2
         }
@@ -23,55 +32,50 @@ function OneBoardOptions() {
     }
 
     function getGridClass() {
-        const boardLength = board.options.length
+        const boardLength = options.length
         if (boardLength === 2 || boardLength === 4) {
             return "grid-margin grid-width-30"
         }
         return "grid-margin grid-width-50"
     }
 
-    const optionsCards = board?.options
+    const moveCard = useCallback((dragIndex, hoverIndex) => {
+        setOptions((prevOptions) =>
+            update(prevOptions, {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, prevOptions[dragIndex]],
+                ],
+            }),
+        )
+    }, [])
+
+    const optionsCards = options
         .map((option, idx) => {
             return (
-                <Grid.Column key={option.id}>
-                    <Card centered className="card-margin">
-                        <div className="card-img-container">
-                            <img
-                                className="card-img"
-                                src={`https://storage.googleapis.com/ranktogether-images/${option.option_image}`} />
-                        </div>
-                        <Card.Content>
-                            <Card.Header>
-                                <div className="board-option-name">
-                                    {option.name}
-                                </div>
-                                <div className="board-option-number">
-                                    <h4> {idx + 1} </h4>
-                                </div>
-                            </Card.Header>
-                        </Card.Content>
-                    </Card>
-                </Grid.Column>
+                <OptionsCards key={option.id} option={option} idx={idx} moveCard={moveCard} />
             )
         })
 
     return (
-        <div>
-            {board === null ? (<Dimmer active>
-                <Loader size='massive'>Loading</Loader>
-            </Dimmer>) :
-                (<>
-                    <div className="header-board">
-                        <Header as='h1'>{board.title}</Header>
-                        <Header as='h2'>{board.description}</Header>
-                        <Header as="h3">{board.user.username}</Header>
-                    </div>
-                    <Grid className={getGridClass()} centered={true}>
-                        <Grid.Row columns={calculateColumns()}>
-                            {optionsCards}
-                        </Grid.Row>
-                    </Grid></>)}
-        </div>
+        <DndProvider backend={HTML5Backend}>
+            <div>
+                {board === null ? (<Dimmer active>
+                    <Loader size='massive'>Loading</Loader>
+                </Dimmer>) :
+                    (<>
+                        <div className="header-board">
+                            <Header as='h1'>{board.title}</Header>
+                            <Header as='h2'>{board.description}</Header>
+                            <Header as="h3">{board.user.username}</Header>
+                        </div>
+                        <Grid className={getGridClass()} centered={true}>
+                            <Grid.Row columns={calculateColumns()}>
+                                {optionsCards}
+                            </Grid.Row>
+                        </Grid></>)}
+            </div>
+        </DndProvider>
     )
 }
 
---
+++ ranktogether/src/Components/OptionsCard.js
@@ -0,0 +1,107 @@
+import React, { useRef } from "react";
+import { Grid, Card, Ref } from "semantic-ui-react"
+import { DragTypes } from '../Types/DragTypes'
+import { useDrag, useDrop } from 'react-dnd'
+
+function OptionsCards({ option, idx, moveCard }) {
+
+	const ref = useRef(null)
+	const [{ handlerId }, drop] = useDrop({
+		accept: DragTypes.CARD,
+		collect(monitor) {
+			return {
+				handlerId: monitor.getHandlerId(),
+			}
+		},
+		hover(item, monitor) {
+			if (!ref.current) {
+				return
+			}
+			const dragIndex = item.index
+			const hoverIndex = idx
+
+			// Don't replace items with themselves
+			if (dragIndex === hoverIndex) {
+				return
+			}
+
+			// Determine rectangle on screen
+			const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
+			// Get vertical middle
+			const hoverMiddleY =
+				(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+			// Determine mouse position
+			const clientOffset = monitor.getClientOffset()
+
+			// Get pixels to the top
+			const hoverClientY = (clientOffset).y - hoverBoundingRect.top
+
+			// Only perform the move when the mouse has crossed half of the items height
+			// When dragging downwards, only move when the cursor is below 50%
+			// When dragging upwards, only move when the cursor is above 50%
+
+			// Dragging downwards
+			if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+				return
+			}
+
+			// Dragging upwards
+			if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+				return
+			}
+
+			// Time to actually perform the action
+			moveCard(dragIndex, hoverIndex)
+
+			// Note: we're mutating the monitor item here!
+			// Generally it's better to avoid mutations,
+			// but it's good here for the sake of performance
+			// to avoid expensive index searches.
+			item.index = hoverIndex
+		},
+	})
+
+	const [{ isDragging }, drag] = useDrag({
+		type: DragTypes.CARD,
+		item: () => {
+			return { id: option.id, index:idx }
+		},
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+	})
+
+	const opacity = isDragging ? 0 : 1
+	drag(drop(ref))
+
+	return (
+		<Ref innerRef={ref}>
+			<Grid.Column
+				data-handler-id={handlerId}>
+				<Card centered className="card-margin">
+					<div className="card-img-container">
+						<img
+							className="card-img"
+							src={`https://storage.googleapis.com/ranktogether-images/${option.option_image}`} />
+					</div>
+					<Card.Content>
+						<Card.Header>
+							<div className="board-option-name">
+								{option.name}
+							</div>
+							<div className="board-option-number">
+								<h4> {idx + 1} </h4>
+							</div>
+						</Card.Header>
+					</Card.Content>
+				</Card>
+			</Grid.Column>
+		</Ref>
+
+	)
+
+}
+
+export default OptionsCards
